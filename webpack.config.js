6c1931eb970aa9d5bf9189a44a22304aac09147c
Jérôme COGNET <jcognet@gmail.com>
2020-11-06 19:04:18 +0100

    Add Vue - close #8

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build-vue/')
+    .setPublicPath('/build-vue')
+    .addEntry('app-vue', './assets-vue/app.js')
+    .enableSassLoader()
+    .enableVueLoader()
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+;
+
+module.exports = Encore.getWebpackConfig();
