6862f89d80b3a08c00529464d3ff4b82530ee9b1
Jérôme COGNET <jcognet@gmail.com>
2020-11-08 14:19:38 +0100

    Add Vue - close #8 (#11)

---
+++ assets-vue/vue/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="main">This is vue ! :)</div>
+</template>
+
+<script>
+export default {
+    name: 'app'
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+    color: #718c00;
+}
+</style>
